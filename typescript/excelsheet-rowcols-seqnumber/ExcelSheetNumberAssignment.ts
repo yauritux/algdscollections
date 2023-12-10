f3dbb63c4da8f10efc81b81d892ce619d395f485
Yauri Attamimi <yauritux@gmail.com>
2023-12-10 20:34:57 +0700

    minor fix on column calculation

--- typescript/excelsheet-rowcols-seqnumber/ExcelSheetNumberAssignment.ts
+++ typescript/excelsheet-rowcols-seqnumber/ExcelSheetNumberAssignment.ts
@@ -1,11 +1,10 @@
 export default class ExcelSheetNumberAssignment {
   parse(row: number, column: string): number {
     let sum = 0;
-    let index = 0;
+    let index = row - 1;
     for (let i = column.length - 1; i >= 0; i--) {
       sum += (column.charCodeAt(i) - 64) * Math.pow(26, index);
-      index++;
     }
-    return sum;
+    return sum + index;
   }
 }
