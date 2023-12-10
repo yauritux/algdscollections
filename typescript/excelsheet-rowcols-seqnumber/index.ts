394d48fefa51b7e5fe6f17c6553dce2fae566de4
Yauri Attamimi <yauritux@gmail.com>
2023-12-10 20:35:11 +0700

    add more samples

--- typescript/excelsheet-rowcols-seqnumber/index.ts
+++ typescript/excelsheet-rowcols-seqnumber/index.ts
@@ -6,3 +6,7 @@ console.log("B1 =", excelNumber.parse(1, "B"));
 console.log("C1 =", excelNumber.parse(1, "C"));
 console.log("D1 =", excelNumber.parse(1, "D"));
 console.log("E1 =", excelNumber.parse(1, "E"));
+console.log("Z1 =", excelNumber.parse(1, "Z"));
+console.log("A2 =", excelNumber.parse(2, "A"));
+console.log("Z2 =", excelNumber.parse(2, "Z"));
+console.log("A3 =", excelNumber.parse(3, "A"));
