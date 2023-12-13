f0d95b839859a12fa6fc9a80ca23e41c70f5e62d
Yauri Attamimi <yauritux@gmail.com>
2023-12-13 23:16:22 +0700

    complete typescript singly linked list ADT for insertions cases variant

--- typescript/linked-list/single/Node.ts
+++ typescript/linked-list/single/Node.ts
@@ -7,9 +7,47 @@ export default class Node {
     this.next = null;
   }
 
+  get val(): string {
+    return this.value;
+  }
+
   append(value: string): Node {
     this.next = new Node(value);
     return this.next;
+  }
+
+  insert(position: number, value: string): Node {
+    console.log("inserting", value, "at position", position);
+    let current: Node = this;
+    if (position > this.length) {
+      let current: Node = this;
+      while (current.next) {
+        current = current.next;
+        if (!current.next) {
+          current.next = new Node(value);
+          break;
+        }
+      }
+      return current;
+    }
+    if (position === 0) {
+      let top = new Node(value);
+      top.next = current;
+      return top;
+    }
+    let i = 0;
+    while (current.next) {
+      if (i === position - 1) {
+        let newNode = new Node(value);
+        let currentActiveNode = current;
+        newNode.next = current.next;
+        currentActiveNode.next = newNode;
+        break;
+      }
+      current = current.next;
+      i++;
+    }
+    return this;
   }
 
   get length(): number {
--- typescript/linked-list/single/index.ts
+++ typescript/linked-list/single/index.ts
@@ -1,6 +1,15 @@
 import Node from "./Node";
 
-const node = new Node("1");
+let node = new Node("1");
 node.append("3").append("5").append("7");
 console.log("Total Nodes=", node.length);
+node.print();
+node.insert(5, "9");
+node.print();
+let newNode: Node = node.insert(0, "4");
+if (newNode.val !== node.val) {
+  node = newNode;
+}
+node.print();
+node.insert(2, "8");
 node.print();
